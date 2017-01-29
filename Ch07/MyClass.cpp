a3eb5ecfb43581225ada21718eb70622d39ec1c3
Develop2017RC3 <Develop@VS2017RC3>
2017-01-30 00:24:25 +0300

    Change #1

--- Ch07/MyClass.cpp
+++ Ch07/MyClass.cpp
@@ -5,3 +5,23 @@
 MyClass::~MyClass()
 {
 }
+
+/// page 167, Delegating Constructors
+/// Delegating constructors allow constructors to call another constructor from the same class. However,
+/// this call cannot be placed in the constructor body; it must be in the constructor initializer and it must
+/// be the only member - initializer in the list
+MyClass::MyClass(const int i, const std::string& s) : MyClass(i)
+{
+	
+}
+
+
+MyClass::MyClass(const std::string& s) : mStr(s), mConstInt(s.size())
+{
+	
+}
+
+MyClass::MyClass(const int i) : mInt(i), mConstInt(i)
+{
+	
+}
--- Ch07/MyClass.h
+++ Ch07/MyClass.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 class MyClass
 {
@@ -7,10 +8,22 @@ public:
 	/// page 159
 	MyClass() = default; // C++11
 
+	MyClass(const int, const std::string&);
+	MyClass(const std::string&);
+	MyClass(const int);
+
 	MyClass(const MyClass& m) = default;
 	//MyClass(const MyClass& m) = delete;
 
 	virtual ~MyClass();
+
+private:
+
+	/// page 167, In-Class Member Initializers, since C++11
+	int mInt = 1;
+	std::string mStr = "test";
+
+	const int mConstInt;
 };
 
 class MyClass2
@@ -20,6 +33,6 @@ public:
 	/// page 160
 	MyClass2() = delete; // C++11
 
-	MyClass2(int i) = delete;
+	explicit MyClass2(int i) = delete;
 };
 
